52f49134a774580a0ffe0d0d1702f7081fd4945a
zenon-provalidator <zenon@provalidator.com>
2022-03-16 11:56:20 +0900

    gg

--- process/telegram_bot.js
+++ process/telegram_bot.js
@@ -67,7 +67,7 @@ bot.command('daemon_restart', async(ctx) => {
 bot.command('balance', async(ctx) => {
 	let text = ctx.update.message.text
 	let parts = text.split(" ")
-	logger.info(parts[1])
+	console.log(parts[1])
 //    const res = await faucet.getBalances(ctx.update.message.text)
 //    logger.info(res)
 //    ctx.reply(res)
